b2b01a9ec202ee4b68eb367eda3c92dcfb0bc474
hadashi <dev@hadashikick.jp>
2012-11-07 00:02:43 +0900

    generate()

--- test/generate.test.js
+++ test/generate.test.js
@@ -1,6 +1,7 @@
-var expect = require('expect.js')
+var expect      = require('expect.js')
   , sexpression = require('../')
-  , generate = sexpression.generate;
+  , generate    = sexpression.generate
+  , intern      = sexpression.Symbol.intern;
 
 describe('generate()', function() {
   describe('number', function() {
@@ -43,12 +44,35 @@ describe('generate()', function() {
     });
 
     it('should to be symbol name', function() {
-      var intern = sexpression.Symbol.intern;
       expect(generate(intern('a'))).to.be(intern('a').toString());
     });
 
     it('should to be cons cell', function() {
       expect(generate({ car: 1, cdr: 2 })).to.be('(1 . 2)');
+    });
+
+    it('should to be null from empty', function() {
+      expect(generate([])).to.be('nil');
+    });
+
+    it('should be number list from number array', function() {
+      expect(generate([1, 2, 3])).to.be('(1 2 3)');
+    });
+
+    it('should be nested list', function() {
+      expect(generate([1, [2, 3], 4, 5])).to.be('(1 (2 3) 4 5)');
+    });
+
+    it('should be string list from string array', function() {
+      expect(generate(["a", "b", "c"])).to.be('("a" "b" "c")');
+    });
+
+    it('should be mixed list from mixed array', function() {
+      expect(generate([1, "abcde", intern('hoge')])).to.be('(1 "abcde" hoge)');
+    });
+
+    it('should be alist from object', function() {
+      expect(generate({ a: 1, b: 2 })).to.be('(("a" . 1) ("b" . 2))');
     });
   });
 });
